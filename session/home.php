fa75331e1c2509c8c58a17f969324a82fa671f85
yarzartun <yarzartun.zotefamily@gmail.com>
2024-02-09 13:28:17 +0630

    add google font

--- session/home.php
+++ session/home.php
@@ -16,6 +16,7 @@
     <title>Home</title>
     <link href="css/style.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 </head>
 <body class="loggedin">
     <!-- <div class="loggedin"> -->
